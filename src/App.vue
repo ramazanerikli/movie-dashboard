839e4c6b46282aadacc7a5e2a3101d66c1f819e8
ramazanerikli <ramazanerikli1@gmail.com>
2023-06-14 16:17:12 +0300

    Bootstrap & sass configuration

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js Appss"/>
+  <button class="btn btn-primary">Bootstrap Button</button>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+@import './styles/main.scss';
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: $primary;
+  margin-top: 60px;
+}
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.css";
+// Custom
+import "./styles/main.scss";
+
+createApp(App).mount('#app')
