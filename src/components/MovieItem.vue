56bb26fda5c08486f18cfe81a32a1c6d0eb763c5
ramazanerikli <ramazanerikli1@gmail.com>
2023-06-17 04:44:35 +0300

    Display circular progress for movie score

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -4,16 +4,25 @@
         <font-awesome-icon icon="fa-solid fa-heart" />
     </div>
     <router-link :to="`/detail/${movie.id}`" class="movie-item-wrapper">
-    <div class="card movie-card">
+      <div class="card movie-card">
           <div class="movie-action">
             <img class="movie-poster" :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`">
-            <span class="vote-average">{{ movie.vote_average }}</span>
+            <div class="vote-average">
+             <svg class="stat-circle" :style="{ '--progress': calculateProgress(movie.score) }" width="150" viewBox="0 0 20 20">
+                <circle class="circle-bg" cx="10" cy="10" r="8" />
+                <circle :class="getProgressClass(movie.vote_average)" cx="10" cy="10" r="8" :style="{ 'stroke-dashoffset': calculateOffset(movie.vote_average) }" />
+                <text class="circle-text" x="50%" y="50%" text-anchor="middle" dominant-baseline="middle">
+                  <tspan class="percentage-text">{{ formatPercentage( movie.vote_average * 10) }}</tspan>
+                  <tspan class="percentage-symbol">%</tspan>
+                </text>
+              </svg>
+            </div>
           </div>
           <div class="card-body">
             <h5 class="movie-title">{{ movie.title }}</h5>
             <p class="release-date text-muted small">{{  movie.release_date  }}</p>
           </div>
-        </div>
+      </div>
       </router-link>
   </div>
 
@@ -22,14 +31,37 @@
 import store from "../../store";
 import {useToast} from 'vue-toast-notification';
 const $toast = useToast();
-
   export default {
     props: {
       movie: {
         required: true
       },
     },
+    computed: {
+      circumference() {
+      const radius = 8;
+      return 2 * Math.PI * radius;
+      },
+    },
     methods: {
+      calculateOffset(score) {
+        const radius = 8;
+      const circumference = 2 * Math.PI * radius;
+      const progress = score / 10; 
+      const offset = circumference * (1 - progress);
+      return `${offset}px`;
+    },
+    calculateProgress(score) {
+      const progress = score * 10; 
+      return `${progress}%`;
+    },
+    getProgressClass(score) {
+      return score > 7 ? 'circle-progress progress-green' : 'circle-progress progress-yellow';
+    },
+    formatPercentage(score) {
+      const roundedPercentage = Math.round(score);
+      return String(roundedPercentage);
+    },
       addToFavorite(movie) {
         const isFavorite = this.$store.getters['isMovieFavorite'](movie.id);
         if(isFavorite) {
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -7,13 +7,22 @@
             <font-awesome-icon icon="fa-solid fa-heart" />
           </div>
           <img class="movie-poster" :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`">
-          <span class="vote-average position-absolute">{{ parseFloat(movie.vote_average).toFixed(1) }}</span>
+          <div class="vote-average position-absolute">
+             <svg class="stat-circle" :style="{ '--progress': calculateProgress(movie.vote_average)}" width="150" viewBox="0 0 20 20">
+                <circle class="circle-bg" cx="10" cy="10" r="8" />
+                <circle :class="getProgressClass(movie.vote_average)" cx="10" cy="10" r="8" :style="{ 'stroke-dashoffset': calculateOffset(movie.vote_average) }" />
+                <text class="circle-text" x="50%" y="50%" text-anchor="middle" dominant-baseline="middle">
+                  <tspan class="percentage-text">{{ formatPercentage( movie.vote_average * 10) }}</tspan>
+                  <tspan class="percentage-symbol">%</tspan>
+                </text>
+              </svg>
+            </div>
         </div>
       </div>
       <div class="col-lg-8 d-flex align-items-center">
         <div class="movie-info-field text-start">
           <h2 class="movie-title">{{movie.title}}</h2>
-          <div class="d-flex movie-meta">
+          <div class="d-flex movie-meta d-flex flex-wrap justify-content-start">
             <span>{{ movie.release_date  }}</span>
             <span class="text-uppercase ms-2 me-2">({{  movie.original_language  }})</span>
             <ul class="d-flex flex-row gap-3 ps-3 pe-1">
@@ -31,7 +40,7 @@
             <h5 class="sub-title my-2">Tagline</h5>
             <span class="fst-italic">{{  movie.tagline  }}</span>
             <h5 class="sub-title my-4">Production Companies</h5>
-            <ul class="companies-list list-unstyled">
+            <ul class="companies-list list-unstyled d-flex flex-wrap justify-content-lg-start justify-content-center justify-content-md-center">
               <li v-for="(company, index) in movie.production_companies" :key="index">
                 <div class="company-logo">
                   <img class="thumb" :src="`https://image.tmdb.org/t/p/w500/${company.logo_path}`" @error="imageLoadError">
@@ -50,16 +59,38 @@ import axios from 'axios';
 import store from "../../store";
 import {useToast} from 'vue-toast-notification';
 const $toast = useToast();
-
  export default{
-  name:'detail',
   data(){
    return{
     id: this.$route.params.Mid,
-    movie: [],
+    movie: []
    }
   },
+  computed: {
+      circumference() {
+      const radius = 8;
+      return 2 * Math.PI * radius;
+      },
+    },
   methods: {
+    calculateOffset(score) {
+        const radius = 8;
+      const circumference = 2 * Math.PI * radius;
+      const progress = score / 10; 
+      const offset = circumference * (1 - progress);
+      return `${offset}px`;
+    },
+    calculateProgress(score) {
+      const progress = score * 10; 
+      return `${progress}%`;
+    },
+    getProgressClass(score) {
+      return score > 7 ? 'circle-progress progress-green' : 'circle-progress progress-yellow';
+    },
+    formatPercentage(score) {
+      const roundedPercentage = Math.round(score);
+      return String(roundedPercentage);
+    },
     imageLoadError (e) {
       e.target.src = require("@/assets/logo.png");
     },
@@ -68,7 +99,7 @@ const $toast = useToast();
       var minutes = a % 60;
       return hours + "h "+minutes + "m";
       },
-      addToFavorite(movie) {
+    addToFavorite(movie) {
         const isFavorite = this.$store.getters['isMovieFavorite'](movie.id);
         if(isFavorite) {
           store.commit('removeFavoriteMovie', movie.id);
@@ -91,15 +122,13 @@ const $toast = useToast();
       },
     },
   mounted() {
-    axios.get
-    (`https://api.themoviedb.org/3/movie/${this.id}?&language=en-US`, {
+    axios.get(`https://api.themoviedb.org/3/movie/${this.id}?&language=en-US`, {
     params: {
       api_key: 'bc3678651b1e8f0bd3ee98d5e1052b24'
     }
   })
   .then(response => {
     this.movie = response.data;
-    console.log(this.movie)
   })
   .catch(error => {
     console.log(error);
