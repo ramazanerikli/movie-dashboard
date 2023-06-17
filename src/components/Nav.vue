76d14b60fccd37fbc505246a57d6aed74a7ee600
ramazanerikli <ramazanerikli1@gmail.com>
2023-06-17 21:21:38 +0300

    Active router link style

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -4,10 +4,10 @@
       <div class="navbar-inner w-100">
       <ul class="navbar-nav mb-2 mb-lg-0">
         <li class="nav-item">
-            <router-link to="/" class="nav-link active">Home</router-link> 
+            <router-link to="/" class="nav-link" active-class="nav-link-active">Home</router-link> 
         </li>
         <li class="nav-item">
-            <router-link to="/favorites" class="nav-link">Favorites</router-link>
+            <router-link to="/favorites" class="nav-link" active-class="nav-link-active">Favorites</router-link>
         </li>
       </ul>
         <SearchBar></SearchBar>
